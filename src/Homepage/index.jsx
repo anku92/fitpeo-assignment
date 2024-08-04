6cd778240b2a9708bed88bfde392b5981b475573
anku92 <138029836+anku92@users.noreply.github.com>
2024-08-04 14:19:38 +0530

    Update Homepage, Sidebar and Header

--- src/Homepage/index.jsx
+++ src/Homepage/index.jsx
@@ -1,11 +1,17 @@
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import './Homepage.css';
 
 const Homepage = () => {
   return (
     <>
       <Header />
       <Sidebar />
+      <div className="homepage">
+        <div className="home-section-left"></div>
+        <div className="home-section-right"></div>
+
+      </div>
     </>
   );
 }
