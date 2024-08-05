b0d79beeb445f3ce7096355795e5ce12f65a66d7
anku92 <138029836+anku92@users.noreply.github.com>
2024-08-05 20:43:01 +0530

    Update BarChart and its properties

--- src/components/BarChart/index.jsx
+++ src/components/BarChart/index.jsx
@@ -1,16 +1,30 @@
+import { Chart as ChartJS } from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 
+import barChartData from '../../data/barChartData.json';
 
 const BarChart = () => {
 
 
+
   return (
-    <div className="row">
-      <div className="col-md-auto">
-        <div className="card">
-          <div className="card-body"></div>
-        </div>
-      </div>
+    <div className="row rounded-4 bg-success m-0">
+
+        <Bar className="py-3"
+          data={{
+            labels: barChartData.map(v => v.label),
+            datasets: [
+              {
+                label: '',
+                data: barChartData.map((e) => e.value),
+                backgroundColor: "#7095FF",
+                borderRadius: 20,
+              }
+            ]
+          }}
+        />
     </div>
+
   );
 }
 
