093e1d3dd5ca3ddef05284377bcc165a6a6d9e06
anku92 <138029836+anku92@users.noreply.github.com>
2024-08-05 20:46:09 +0530

    Improved DashboardMiniGrid logic for dynamic rendering of local data

--- src/components/DashboardMiniGrid/index.jsx
+++ src/components/DashboardMiniGrid/index.jsx
@@ -8,44 +8,44 @@ const DashboardMiniGrid = () => {
     {
       icon: "https://placehold.co/200/283169/png",
       title: "Total Orders",
-      value: {count: '75'},
+      value: { count: '75' },
       status: 'up',
       change: 3
     },
     {
       icon: "https://placehold.co/200/145347/png",
       title: "Total Delivered",
-      value: {count: '70'},
+      value: { count: '70' },
       status: 'down',
       change: 3
     },
     {
       icon: "https://placehold.co/200/613339/png",
       title: "Total Cancelled",
-      value: {count :'05'},
+      value: { count: '05' },
       status: 'up',
       change: 3
     },
     {
       icon: "https://placehold.co/200/5D2B4A/png",
       title: "Total Revenue",
-      value: {amount: '12'},
+      value: { amount: '12' },
       status: 'down',
       change: 3
     }
   ]
 
   return (
-    
-      <div className="row">
-        {
-          grid_data.map((gdata, index) => {
-            return <Grid key={index} data={gdata} />
-          })
-        }
-      </div>
 
-    
+    <div className="row p-0 m-0">
+      {
+        grid_data.map((gdata, index) => {
+          return <Grid key={index} data={gdata} />
+        })
+      }
+    </div>
+
+
   );
 }
 
