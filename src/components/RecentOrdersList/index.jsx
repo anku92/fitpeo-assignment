6bb5e86bd77591db4dc46f051f68540883faf05a
anku92 <138029836+anku92@users.noreply.github.com>
2024-08-07 20:04:24 +0530

    Add checkpoint

--- src/components/RecentOrdersList/index.jsx
+++ src/components/RecentOrdersList/index.jsx
@@ -7,36 +7,37 @@ const RecentOrdersList = () => {
     if (status === "Delivered") return 'success';
     return 'no-success';
   }
-
+  // note for self: Design the table components all is set
   return (
-// p-0 mt-0 mb-3 mx-0
-    <div style={{ backgroundColor: "var(--cs-dark-blue)" }} className="row rounded-3 mx-1 px-4">
-      <h3 className="px-0 py-3 m-0">Recent Orders</h3>
-      <table className="custom-table">
-        <thead>
-          <tr>
-            <th>Customer</th>
-            <th>Order No.</th>
-            <th>Amount</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ordersData.map((e, index) =>
-            <tr key={index}>
-              <td>
-                <img src={e.image} className='customer-image' />
-                {e.name}
-              </td>
-              <td>{e.order_number}</td>
-              <td>${e.amount}</td>
-              <td>
-                <span className={`status ${findStatus(e.status)}`}>{e.status}</span>
-              </td>
+    <div className="row bg-danger p-1 mt-0 mb-3 mx-0">
+      <div className="table-container">
+        <h3 className="p-0 m-0">Recent Orders</h3>
+        <table className="custom-table">
+          <thead>
+            <tr>
+              <th>Customer</th>
+              <th>Order No.</th>
+              <th>Amount</th>
+              <th>Status</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {ordersData.map((e, index) =>
+              <tr key={index}>
+                <td>
+                  <img src={e.image} className='customer-image' />
+                  {e.name}
+                </td>
+                <td>{e.order_number}</td>
+                <td>${e.amount}</td>
+                <td>
+                  <span className={`status ${findStatus(e.status)}`}>{e.status}</span>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
 
   )
