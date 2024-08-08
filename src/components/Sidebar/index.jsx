464b08942102f0494942e65bc80ac455cd5cc102
anku92 <138029836+anku92@users.noreply.github.com>
2024-08-08 15:47:06 +0530

    Replace div to Link, add logout icon, remove sm-logo-container

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -5,6 +5,8 @@ import { AiFillHome } from 'react-icons/ai';
 import { RiBarChartBoxLine } from 'react-icons/ri';
 import { BsClipboard2Check, BsBagCheck } from 'react-icons/bs';
 import { CiWallet } from 'react-icons/ci';
+import { RiLogoutCircleRLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
   const [activeIcon, setActiveIcon] = useState('home');
@@ -15,53 +17,58 @@ function Sidebar() {
 
   return (
     <div className="sidebar">
-      <div className="lg-logo-container">
-        <Logo size={'30px'} />
-      </div>
-
-      <div className="sm-logo-container">
-        <Logo size={'25px'} />
-      </div>
+      <div className="logo-container"><Logo size={'30px'} /></div>
 
       <div className="navigation-icons">
-        <div
+        <Link
+          to="/"
           className={`icon-container ${activeIcon === 'home' ? 'active' : ''}`}
           onClick={() => handleIconClick('home')}
         >
           <AiFillHome />
-        </div>
+        </Link>
 
-        <div
+        <Link
+          to="/"
           className={`icon-container ${activeIcon === 'graph' ? 'active' : ''}`}
           onClick={() => handleIconClick('graph')}
         >
           <RiBarChartBoxLine />
-        </div>
+        </Link>
 
-        <div
+        <Link
+          to="/"
           className={`icon-container ${activeIcon === 'clipboard' ? 'active' : ''}`}
           onClick={() => handleIconClick('clipboard')}
         >
           <BsClipboard2Check />
-        </div>
+        </Link>
 
-        <div
+        <Link
+          to="/"
           className={`icon-container ${activeIcon === 'wallet' ? 'active' : ''}`}
           onClick={() => handleIconClick('wallet')}
         >
           <CiWallet />
-        </div>
+        </Link>
 
-        <div
+        <Link
+          to="/"
           className={`icon-container ${activeIcon === 'bagcheck' ? 'active' : ''}`}
           onClick={() => handleIconClick('bagcheck')}
         >
           <BsBagCheck />
-        </div>
+        </Link>
       </div>
+
+      <div className="logout-icon-container">
+        <Link to="/" className="icon-container">
+          <RiLogoutCircleRLine />
+        </Link>
+      </div>
+
     </div>
   );
 }
 
 export default Sidebar;
-
